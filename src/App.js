1bac3a167c4c3408cb18e0a635011ab8e9fa142b
profnoah <noah@clarusway.com>
2021-12-16 20:45:02 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import About from "./pages/About";
+import Header from "./pages/Header";
+
 function App() {
   return (
     <div>
       <h1>App</h1>
+      <Header />
+      <About />
     </div>
   );
 }
